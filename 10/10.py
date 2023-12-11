536eafb3b6a9f002cc39d3b89153bf2690b1276b
whoispim <pimbot@bk.ru>
2023-12-11 16:06:53 +0100

    day10

--- 10/10.py
+++ 10/10.py
@@ -1,5 +1,27 @@
 import numpy as np
 
+# beware, i didn't have a lot of time and this is one bowl of spaghetti
+
+def get_neighbours(pos, dir):
+    if   dir == 0b0001:
+        if   pipes[pos] == 0b1001: return [[(-1,0)],[(1,0)]]
+        elif pipes[pos] == 0b1010: return [[],[(1,0),(0,1)]]
+        elif pipes[pos] == 0b1100: return [[(-1,0),(0,1)],[]]
+    elif dir == 0b0010:
+        if   pipes[pos] == 0b0101: return [[(-1,0),(0,-1)],[]]
+        elif pipes[pos] == 0b0110: return [[(0,-1)],[(0,1)]]
+        elif pipes[pos] == 0b1100: return [[],[(-1,0),(0,1)]]
+    elif dir == 0b0100:
+        if   pipes[pos] == 0b0011: return [[],[(1,0),(0,-1)]]
+        elif pipes[pos] == 0b0110: return [[(0,1)],[(0,-1)]]
+        elif pipes[pos] == 0b1010: return [[(1,0),(0,1)],[]]
+    elif dir == 0b1000:
+        if   pipes[pos] == 0b0011: return [[(-1,0),(0,1)],[]]
+        elif pipes[pos] == 0b0101: return [[],[(-1,0),(0,-1)]]
+        elif pipes[pos] == 0b1001: return [[(1,0)],[(-1,0)]]
+    return [[],[]]
+
+
 #          <v^>
 con_dic = {
     '|': 0b0110,
@@ -48,28 +70,34 @@ dir_left_right = {0b0001: (0b0010, 0b0100),
 n = 0
 dist = {pos: 0}
 neighbours = {}
+neighbours_l = []
+neighbours_r = []
 while True:
     n += 1
     pos = tuple(np.add(pos, dir_to_np[dir]))
     dist[pos] = n
-    neighbours[pos] = [
-        tuple(np.add(pos, dir_to_np[dir_left_right[dir][0]])),
-        tuple(np.add(pos, dir_to_np[dir_left_right[dir][1]]))
-    ]
+    lr = get_neighbours(pos, dir)
+    neighbours_l.extend([(pos[0]+x[0],pos[1]+x[1]) for x in lr[0]]) 
+    neighbours_r.extend([(pos[0]+x[0],pos[1]+x[1]) for x in lr[1]])
+    # neighbours[pos] = [
+    #     tuple(np.add(pos, dir_to_np[dir_left_right[dir][0]])),
+    #     tuple(np.add(pos, dir_to_np[dir_left_right[dir][1]]))
+    # ]
     dir = pipes[pos] ^ dir_to_from[dir]
     if pipes[pos] == 15:
         break
     
 print(f'a: The farthest point is {n//2} steps away from the start.')
 
-# if we circled right, more right neighbours will overlap the path
-r = sum(1 for x in neighbours.values() if x[1] in dist.keys())
-l = sum(1 for x in neighbours.values() if x[0] in dist.keys())
-print(l, r)
+# if we circled right, their will be more left neighbours
+r = len(neighbours_r)
+l = len(neighbours_l)
+# print(l, r)
+
 if r > l:
-    inner_neighbours = [x[1] for x in neighbours.values()]
+    inner_neighbours = [x for x in neighbours_l]
 else:
-    inner_neighbours = [x[0] for x in neighbours.values()]
+    inner_neighbours = [x for x in neighbours_r]
 inner_candidates = [x for x in inner_neighbours]
 inner_empty = []
 while len(inner_candidates) > 0:
@@ -82,31 +110,25 @@ while len(inner_candidates) > 0:
             (coord[0], coord[1]-1),
             (coord[0]-1, coord[1])
         ])
-# print(inner_empty)
-# > 427
-print(len(inner_empty))
-plot_dict = {v: k for k, v in con_dic.items()}
-a=0
-for i in range(pipes.shape[0]):
-    for j in range(pipes.shape[1]):
-        if (i,j) in dist.keys():
-            print(plot_dict[pipes[i,j]], end='')
-        elif (i,j) in inner_empty:
-            print('█', end='')
-            a+=1
-        else:
-            print(' ', end='')
-    print(a)
+        
+print(f'b: The total enclosed area is {len(inner_empty)}.')
 
-for i in range(pipes.shape[0]):
-    for j in range(pipes.shape[1]):
-        if (i,j) in dist.keys():
-            print(plot_dict[pipes[i,j]], end='')
-        elif ((i+1,j) in dist.keys()
-              or (i-1,j) in dist.keys()
-              or (i,j+1) in dist.keys()
-              or (i,j-1) in dist.keys()) and (i,j) not in inner_empty:
-            print('█', end='')
-        else:
-            print(' ', end='')
-    print()
+plot_dict = {0b0110 :"┃",
+             0b1001:"━",
+             0b0011:"┗",
+             0b1010:"┛",
+             0b1100:"┓",
+             0b0101:"┏",
+             0b1111:"S"
+}
+# a=0
+# for i in range(pipes.shape[0]):
+#     for j in range(pipes.shape[1]):
+#         if (i,j) in dist.keys():
+#             print(plot_dict[pipes[i,j]], end='')
+#         elif (i,j) in inner_empty:
+#             print('█', end='')
+#             a+=1
+#         else:
+#             print('.', end='')
+#     print(a)
