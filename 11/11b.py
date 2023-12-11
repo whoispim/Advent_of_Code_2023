de49bc4948bfa5b90cfbf94bdddec427c7730726
whoispim <pimbot@bk.ru>
2023-12-11 22:36:42 +0100

    day11

--- 11/11b.py
+++ 11/11b.py
@@ -1,4 +1,4 @@
-with open('11/input_t.txt','r') as f:
+with open('11/input.txt','r') as f:
     stars = []
     for i, line in enumerate(f.read().strip().split('\n')):
         for j, ch in enumerate(line):
@@ -8,16 +8,19 @@ max_r = max(x[0] for x in stars)
 max_c = max(x[1] for x in stars)
 empty_rows = [x for x in range(max_r+1) if x not in [x[0] for x in stars]]
 empty_cols = [x for x in range(max_c+1) if x not in [x[1] for x in stars]]
-print(stars)
-print(empty_rows)
-print(empty_cols)
+
 for s in stars:
     for r in reversed(empty_rows):
         if s[0] > r:
-            s[0] += 1
+            s[0] += 999999
     for c in reversed(empty_cols):
         if s[1] > c:
-            s[1] += 1
+            s[1] += 999999
 
-
-print(stars)
+sum_dist = 0
+for i, s_a in enumerate(stars):
+    for s_b in stars[i+1:]:
+        sum_dist += abs(s_a[0]-s_b[0]) + abs(s_a[1]-s_b[1])
+        
+print(f'b: The sum of the distance between all',
+      f'stars is {sum_dist} after expansion by 999999.')
