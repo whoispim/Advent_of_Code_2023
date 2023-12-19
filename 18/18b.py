ee7dbe31b248492410ee6d2bcd2e2ca9ad0c6343
whoispim <pimbot@bk.ru>
2023-12-19 16:41:56 +0100

    day18b start

--- 18/18b.py
+++ 18/18b.py
@@ -1,35 +1,39 @@
-with open('18/input.txt', 'r') as f:
+with open('18/input_t.txt', 'r') as f:
     instructions = []
     for line in f.read().strip().split('\n'):
-        direction, distance, _color = line.split()
-        instructions.append((direction, int(distance)))
-        # int('string', 16)
+        direction = int(line[11])
+        distance = int(line[6:11], 16)
+        instructions.append((direction, distance))
         
 dir_dict = {
-    'R': ( 0, 1),
-    'D': ( 1, 0),
-    'L': ( 0,-1),
-    'U': (-1, 0)
+    0: ( 0, 1),
+    1: ( 1, 0),
+    2: ( 0,-1),
+    3: (-1, 0)
     }
 
 print(instructions)
-pos = (0,0)
-dug = [(0,0)]
-in_out_walls = [(0,0)]
-last_dir = 'U'
+
+dug_corners = [(0,0)]
+
 for inst in instructions:
     pos_dif = dir_dict[inst[0]]
-    for n in range(inst[1]):
-        pos = (pos[0] + pos_dif[0], pos[1] + pos_dif[1])
-        dug.append(pos)
-        if inst[0] == 'U' or inst[0] == 'D':
-            if last_dir != inst[0]:
-                _ = in_out_walls.pop()
-                last_dir = inst[0]
-            in_out_walls.append(pos)
-if in_out_walls[-1] == (0,0):
-    _ = in_out_walls.pop()
+    new_corner = (dug_corners[-1][0] + pos_dif[0] * inst[1],
+                    dug_corners[-1][1] + pos_dif[1] * inst[1])
+    dug_corners.append(new_corner)
+print(dug_corners)
 
+# shoelace formula
+lagoon = 0
+for i in range(len(dug_corners) -1):
+    a = dug_corners[i]
+    b = dug_corners[i + 1]
+    print(a,'|||',b, '---->', a[0]*b[1] - a[1]*b[0] )
+    lagoon += a[0]*b[1] - a[1]*b[0]
+lagoon = abs(lagoon)
+print(lagoon)
+# way too much
+exit()
 print(dug)
 ij_min = (min(x[0] for x in dug), min(x[1] for x in dug))
 ij_max = (max(x[0] for x in dug), max(x[1] for x in dug))
